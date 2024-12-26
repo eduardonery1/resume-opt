230380ea687b1cfc53fd843b843a84069687d8a2
Eduardo Nery <eduardo.nery8@gmail.com>
2024-12-26 19:20:32 -0300

    basic back-end infrastructure modules

---
+++ back-end/consumer.py
@@ -0,0 +1,39 @@
+import os
+import json
+from db import db
+import asyncio
+from aio_pika import connect, IncomingMessage
+import json
+
+
+user = os.environ["RABBITMQ_USER"]
+password  = os.environ["RABBITMQ_password"]
+host = os.environ["RABBITMQ_HOST"]
+port = os.environ["RABBITMQ_PORT"]
+
+queue_name= "apiGateAway"
+
+url = f'amqp://{user}:{password}@{host}:{port}/%2F'
+
+
+async def callback(message: IncomingMessage):
+    print('recieved a massage in api gateaway')
+    txt = message.body.decode("utf-8")
+    data = json.loads(txt)
+    print(data)
+    await db["preds"].insert_one(data)
+    print(f'|+| saved new pollution {data}')
+
+
+async def main(loop):
+    connection = await connect(url, loop = loop)
+    channel = await connection.channel()
+    queue = await channel.declare_queue(queue_name)
+    await queue.consume(callback, no_ack = True)
+    print("start consuming ")
+
+
+if __name__=="__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(main(loop))
+    loop.run_forever()
