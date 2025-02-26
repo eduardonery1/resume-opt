f2b62dda696770298d8656955164d091cddf2bc7
Luís Paulo Trevisan <lpst@cin.ufpe.br>
2025-02-26 15:05:57 -0300

    fix: width of button

--- front-end/src/app/optimizer/page.js
+++ front-end/src/app/optimizer/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Stepper from "../components/stepper";
@@ -10,9 +10,11 @@ export default function Optimizer() {
   return (
     <main className="flex flex-col items-center h-full justify-start">
       <Stepper currStep={currStep} steps={steps} />
+      
       <div className="w-3/5 min-h-[60vh] shadow-[4px_4px_20px_0px_var(--shadow)]"></div>
+      
       <div className="flex justify-between items-center m-4 h-12 w-3/5 gap-12">
-        <button className={(currStep > 0) ? "border-none h-full text-xl font-semibold text-[var(--background)] bg-[var(--stepper-curr)] flex-[0.5_0_1]" : "border-none h-full text-xl font-semibold bg-[var(--stepper)] flex-[0.5_0_1] text-[var(--background)]"} onClick={() => {
+        <button className={(currStep > 0) ? "border-none h-full w-1/2 text-xl font-semibold text-[var(--background)] bg-[var(--stepper-curr)] flex-[0.5_0_1]" : "border-none h-full w-1/2 text-xl font-semibold bg-[var(--stepper)] flex-[0.5_0_1] text-[var(--background)]"} onClick={() => {
           setCurrStep(prevState => {
             return (prevState > 0) ? prevState - 1 : 0;
           })
@@ -20,7 +22,7 @@ export default function Optimizer() {
           Back
         </button>
 
-        <button className={(currStep < steps.length - 1) ? "border-none h-full text-xl font-semibold text-[var(--background)] bg-[var(--stepper-curr)] flex-[0.5_0_1]" : "border-none h-full text-xl font-semibold bg-[var(--stepper)] flex-[0.5_0_1] text-[var(--background)]"} onClick={() => {
+        <button className={(currStep < steps.length - 1) ? "border-none h-full w-1/2 text-xl font-semibold text-[var(--background)] bg-[var(--stepper-curr)] flex-[0.5_0_1]" : "border-none h-full w-1/2 text-xl font-semibold bg-[var(--stepper)] flex-[0.5_0_1] text-[var(--background)]"} onClick={() => {
           setCurrStep(prevState => {
             return (prevState < steps.length - 1) ? prevState + 1 : steps.length - 1;
           })
